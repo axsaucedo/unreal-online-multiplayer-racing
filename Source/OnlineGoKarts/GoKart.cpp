fbebeb2274c0145c529a2c5e773618ba00d3885e
Alejandro Saucedo <axsauze@gmail.com>
2020-06-07 09:19:45 +0100

    Basic movements

--- Source/OnlineGoKarts/GoKart.cpp
+++ Source/OnlineGoKarts/GoKart.cpp
@@ -1,14 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "GoKart.h"
+
+#include "Components/InputComponent.h"
 
 // Sets default values
 AGoKart::AGoKart()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -23,6 +23,14 @@ void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector Translation = Velocity * DeltaTime * 100;
+
+	AddActorWorldOffset(Translation);
+}
+
+void AGoKart::MoveForward(float Value)
+{
+	Velocity = GetActorForwardVector() * 20 * Value;
 }
 
 // Called to bind functionality to input
@@ -30,5 +38,6 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
 }
 
